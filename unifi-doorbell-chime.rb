bb44e8d85e5020a5f3e23d2fd3fe620b1a547b93
Shota Sawada <shota@sslife.tech>
2020-10-28 20:32:14 +0900

    Brew formula update for unifi-doorbell-chime version 0.0.1

--- unifi-doorbell-chime.rb
+++ unifi-doorbell-chime.rb
@@ -7,7 +7,7 @@ class UnifiDoorbellChime < Formula
 
   if OS.mac?
     url "https://github.com/sawadashota/unifi-doorbell-chime/releases/download/0.0.1/unifi-doorbell-chime_0.0.1_Darwin_x86_64.tar.gz"
-    sha256 "37680a0290850e2573dcd1edff1d38df0f9bd3effff2133f7afc4fed766e06f2"
+    sha256 "b37e07791093682ab9ce2858dc4f7e5e0b3bde7db7f97dc4d540f33e7dd24961"
   elsif OS.linux?
   end
 
@@ -17,7 +17,7 @@ class UnifiDoorbellChime < Formula
     puts "please exec init command"
     puts
     puts
-    puts "$ unifi-proto-chime init"
+    puts "$ unifi-doorbell-chime init"
     puts
     puts
   end
