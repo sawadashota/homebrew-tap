00cac29a3a2d50b3b502c62279114c5c063212c8
sawadashota <shota@sslife.tech>
2022-01-11 21:40:56 +0900

    unifi-doorbell-notifier 0.1.6
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- unifi-doorbell-notifier.rb
+++ unifi-doorbell-notifier.rb
@@ -1,10 +1,10 @@
 class UnifiDoorbellNotifier < Formula
   desc "Notify to your PC when Doorbell rung."
   homepage "https://github.com/sawadashota/unifi-doorbell-notifier"
-  version "0.1.5-beta"
+  version "0.1.6"
 
-  url "https://github.com/sawadashota/unifi-doorbell-notifier/releases/download/v0.1.5-beta/unifi-doorbell-notifier_v0.1.5-beta_x86_64-apple-darwin.zip"
-  sha256 "8fc11b4d67aee7107f7fa5531cd5b713c9abc9886d9abc6090bde0c31aab74db"
+  url "https://github.com/sawadashota/unifi-doorbell-notifier/releases/download/v0.1.6/unifi-doorbell-notifier_v0.1.6_x86_64-apple-darwin.zip"
+  sha256 "c9e63c54934db3c1f575ec0178870fef263da9e77e861bd56de4d6588795c7b7"
 
   def install
     bin.install "unifi-doorbell-notifier"
